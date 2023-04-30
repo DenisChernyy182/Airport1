ffd83a9aceed336f83c8fd9070822aa40397ef56
DenisChernyy182 <chernyasha94@mail.ru>
2023-04-30 18:58:50 +0300

    homework 3 done

--- ObjectsAndClasses_Methods/src/Arithmetic.java
+++ ObjectsAndClasses_Methods/src/Arithmetic.java
@@ -1,3 +1,20 @@
 public class Arithmetic {
 
-}
+        int parametr1 = 2;
+        int parametr2 = 3;
+
+    public Arithmetic( int parametr3, int parametr4){
+            parametr1 = parametr3;
+            parametr2 = parametr4;
+
+            System.out.println("сумму чисел = " + (parametr3 + parametr4));
+            System.out.println("производная чисел чисел = " + (parametr3 * parametr4));
+
+            if (parametr3 > parametr4) {
+                System.out.println("максимальное из двух чисел: " + parametr3);
+            } else {
+                System.out.println("минимальное из двух чисел: " + parametr3);
+            }
+        }
+    }
+
