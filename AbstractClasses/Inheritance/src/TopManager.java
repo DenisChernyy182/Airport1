b942e0094f6dc4bcda269b9aa5d2bc52d7631f13
DenisChernyy182 <chernyasha94@mail.ru>
2023-08-13 21:07:05 +0300

    Merge remote-tracking branch 'origin/master'

--- AbstractClasses/Inheritance/src/TopManager.java
+++ AbstractClasses/Inheritance/src/TopManager.java
@@ -1,13 +1,13 @@
 public class TopManager implements Employee {
     private static final int SALARY = 100000;
 
-    private static final int minCompanyIncome = 1_000_000;
-    private static final double bonusTopManager = 1.5;
+    private static final int MIN_COMPANY_INCOME = 1_000_000;
+    private static final double BONUS_TOP_MANAGER = 1.5;
 
     @Override
     public int getMonthSalary() {
-        if (Company.getIncome() + SALARY > minCompanyIncome) {
-            return (int) (SALARY * bonusTopManager);
+        if (getMonthSalary() + SALARY > MIN_COMPANY_INCOME) {
+            return (int) (SALARY * BONUS_TOP_MANAGER);
         } else {
             return SALARY;
         }
