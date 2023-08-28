2ec78587bcc9b8cedc9903b710af6ed28905a62c
Skillbox <it@skillbox.ru>
2023-08-28 17:39:17 +0300

    Add lesson

---
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -0,0 +1,33 @@
+import com.skillbox.airport.Airport;
+import com.skillbox.airport.Flight;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main (String[] args) {
+
+
+    }
+
+    public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
+        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
+
+        LocalDateTime dateNow = LocalDateTime.now();
+        return airport.getTerminals().stream()
+                .flatMap(terminal -> terminal.getFlights().stream())
+                .filter(flight -> flight.getType().equals(Flight.Type.DEPARTURE))
+                .filter(flight -> modifiedDate(flight)
+                        .isAfter(dateNow) && modifiedDate(flight)
+                        .isBefore(dateNow.plusHours(2)))
+                .collect(Collectors.toList());
+    }
+
+    public static LocalDateTime modifiedDate(Flight date) {
+        return LocalDateTime.ofInstant(date.getDate().toInstant(), ZoneId.systemDefault());
+    }
+
+}
