2ee7831de12e0589d79372ba7b31140ca06d7f41
DenisChernyy182 <chernyasha94@mail.ru>
2023-09-24 14:19:46 +0300

    homework

--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
@@ -2,10 +2,15 @@ import java.io.FilterWriter;
 import java.util.Scanner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
 
 public class Main {
 
     private static Logger logger;
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+    private static final Marker INPUT_HISTORY_MARKER = MarkerManager.getMarker("INPUT_HISTORY");
+    private static final Marker INVALID_STATIONS_MARKER = org.apache.logging.log4j.MarkerManager.getMarker("INVALID_STATIONS");
     private static final String ADD_COMMAND = "add Василий Петров " +
             "vasily.petrov@gmail.com +79215637722";
     private static final String COMMAND_EXAMPLES = "\t" + ADD_COMMAND + "\n" +
@@ -17,8 +22,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         CustomerStorage executor = new CustomerStorage();
-        logger = LogManager.getRootLogger();
-        logger.info("test");
+//        logger = LogManager.getRootLogger();
+//        logger.info("test");
+//        logger.error("Ошибка добовления");
+
+        LOGGER.info(INPUT_HISTORY_MARKER, "Пользователь данные: {}", scanner);
+        LOGGER.info(INVALID_STATIONS_MARKER, "Ошибка, пользователь ввел: {}", scanner);
 
         while (true) {
             String command = scanner.nextLine();
